780e786539b0f33eda3f1d51a07d65718e2befec
unknown <fazahuanchef@gmail.com>
2022-01-12 20:40:47 -0500

    read a file and print on console

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = () => {
   // ...
 };
+const fs = require("fs"),
+  FILE_NAME = "./archivo.txt";
+
+fs.readFile(FILE_NAME, 'utf8', (error, text) => {
+    if (error) throw error;
+    console.log("The content is: ", text);
+});
