22249d6e1e28d11602a57eaf64f306b5537abc28
unknown <fazahuanchef@gmail.com>
2022-01-15 02:58:19 -0500

    resolving if the path exists

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const path = require("path");
 // Import the filesystem module
 const fs = require("fs");
+const { exit } = require("process");
 
 const absolutePathFile = "C:/Users/fiorela/Desktop/Laboratoria Lim016/proyectos/cuartoProyecto/LIM016-md-links/sampleFiles/README.md";
 const relativePathFile = "./sampleFiles/README.md";
@@ -10,14 +11,20 @@ const relativePathDirectory = "./sampleFiles/";
 // The path.isAbsolute() method determines if path is an absolute path.
 const convertToPathAbsolute = (inputPath) => {
   if (path.isAbsolute(inputPath)) {
-    console.log('Es absoluta, la ruta es:');
+    console.log("It is absolute, the path is: ");
     return inputPath;
   } else {
     //The path.resolve() method resolves a sequence of paths or path segments into an absolute path.
-    console.log('Es relativa, la ruta absoluta sería: ');
+    console.log("It is relative, the absolute path would be: ");
     return path.resolve(inputPath);
   }
 };
+
+// validate if the path exists
+const verifiedPathExist = (inputPath) =>{
+  const pathExist = fs.existsSync(inputPath)
+  return pathExist;
+}
 
 // Getting information for a file
 statsObj = fs.statSync(absolutePathDirectory);
@@ -27,3 +34,6 @@ console.log("Path is directory:", statsObj.isDirectory());
 
 showPath = convertToPathAbsolute(relativePathFile);
 console.log(showPath);
+
+existsPath = verifiedPathExist(relativePathDirectory);
+console.log("Does the path exist? ",existsPath);
